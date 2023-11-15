3c2ae95e8820e3d1d6f5a54c834a3a9268908726
Aloka Devapriya <28234480+Alokadev@users.noreply.github.com>
2023-11-15 22:41:08 +0530

    Sign in Singup
    Just started to sign in and up pages.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,28 @@
+import AuthLayout from './_auth/forms/AuthLayout';
+import SigninForm from './_auth/forms/SigninForm';
+import SignupForm from './_auth/forms/SignupForm';
+import { Home } from './_root/pages';
+import RootLayout from './_root/pages/RootLayout';
+import './globals.css';
+import { Routes, Route } from 'react-router-dom';
+
 const App = () => {
   return (
-    <div>App</div>
+    <main className='flex h-screen'>
+        <Routes>
+            {/* public routes */}
+            <Route element={<AuthLayout />}>
+              <Route path='/sign-in' element={<SigninForm/>}/>
+              <Route path='/sign-up' element={<SignupForm/>}/>
+            </Route>
+
+            {/* public routes */}
+            <Route element={<RootLayout />}>
+              <Route index element={<Home/>}/>
+            </Route>
+            
+        </Routes>
+    </main>
   )
 }
 
